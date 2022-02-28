645fd8eae59161f8be710c40638dcca4e0f423c9
Shubham Srivastava <smjackson94@gmail.com>
2022-02-28 10:00:03 +0530

    removed private methods

--- App.js
+++ App.js
@@ -16,9 +16,9 @@ const conf = {
 
 export default class Fibotalk {
 
-  static #xhr = new XMLHttpRequest();
-  #storage = {};
-  static #device = {};
+  static xhr = new XMLHttpRequest();
+  storage = {};
+  static device = {};
   fibotalkSettings;
   appid;
 
@@ -29,12 +29,13 @@ export default class Fibotalk {
    * @returns 
    */
   constructor(appid) {
+    console.log("Fibotalk: starting", appid);
     if (!appid)
       return null;
     this.appid = appid;
-    this.#store("get").then(resp => {
-      this.#storage = resp || this.#storage;
-      this.#init();
+    this.store("get").then(resp => {
+      this.storage = resp || this.storage;
+      this.init();
     }).catch(error => {
       console.error(error);
     });
@@ -48,39 +49,39 @@ export default class Fibotalk {
    *    - If not present, gen new session.
    * Get device info and store it.
    */
-  #init() {
+  init() {
     let newSess = false;
-    if (!this.#storage.uid) {
-      this.#storage.uid = Fibotalk.#genId(conf.uidLen);
+    if (!this.storage.uid) {
+      this.storage.uid = Fibotalk.genId(conf.uidLen);
       newSess = true;
     }
-    if (!newSess && !this.#storage.session)
+    if (!newSess && !this.storage.session)
       newSess = true;
-    if (!newSess && this.#checkSessionChange())
+    if (!newSess && this.checkSessionChange())
       newSess = true;
     if (newSess)
-      return this.#genSession().then(resp => {
-        Fibotalk.#initSystemData();
+      return this.genSession().then(resp => {
+        Fibotalk.initSystemData();
       }).catch(err => {
         console.error("Fibotalk error: ", err);
-        this.#exit();
+        this.exit();
       });
-    Fibotalk.#initSystemData();
+    Fibotalk.initSystemData();
   }
 
   /**
    * Get device info and store it.
    */
-  static #initSystemData() {
+  static initSystemData() {
     const { height, width } = useWindowDimensions();
-    this.#device = this.#device || {};
-    this.#device["device#screenHeight"] = height;
-    this.#device["device#screenWidth"] = width;
-    this.#device["website"] = DeviceInfo.getApplicationName();
-    DeviceInfo.getDeviceName().then((deviceName) => { this.#device["browser"] = deviceName }).catch(err => console.error(err));
-    this.#device["device"] = DeviceInfo.getDeviceType();
-    this.#device["platform"] = DeviceInfo.getSystemName();
-    this.#device["osVersion"] = DeviceInfo.getSystemVersion();
+    this.device = this.device || {};
+    this.device["device#screenHeight"] = height;
+    this.device["device#screenWidth"] = width;
+    this.device["website"] = DeviceInfo.getApplicationName();
+    DeviceInfo.getDeviceName().then((deviceName) => { this.device["browser"] = deviceName }).catch(err => console.error(err));
+    this.device["device"] = DeviceInfo.getDeviceType();
+    this.device["platform"] = DeviceInfo.getSystemName();
+    this.device["osVersion"] = DeviceInfo.getSystemVersion();
   }
 
   /**
@@ -89,13 +90,13 @@ export default class Fibotalk {
    * 2. Check number of events in session.
    * 3. Check for user change.
    */
-  #checkSessionChange() {
+  checkSessionChange() {
     try {
-      if (this.#storage.lastEventTs && (new Date(this.#storage.lastEventTs).getTime() - Date.now() > conf.awayTime))
+      if (this.storage.lastEventTs && (new Date(this.storage.lastEventTs).getTime() - Date.now() > conf.awayTime))
         return true;
-      if (this.#storage.eventCount >= conf.maxEventsInSession)
+      if (this.storage.eventCount >= conf.maxEventsInSession)
         return true;
-      if (this.#userChanged())
+      if (this.userChanged())
         return true;
     } catch (error) {
       return true;
@@ -106,20 +107,20 @@ export default class Fibotalk {
   /** -------------------------------------
    * compare window.fibotalkSettings with user data in localStorage. 
    -------------------------------------*/
-  #userChanged() {
-    if (!(this.fibotalkSettings && this.#storage.user))
+  userChanged() {
+    if (!(this.fibotalkSettings && this.storage.user))
       return false;
     var userId = this.fibotalkSettings.userId;
     if (this.fibotalkSettings.user)
       userId = this.fibotalkSettings.user.userId || userId;
-    if (Fibotalk.#isObjOrFunc(userId))
+    if (Fibotalk.isObjOrFunc(userId))
       return false;
-    if (this.#storage.user.userId && userId && this.#storage.user.userId != userId)
+    if (this.storage.user.userId && userId && this.storage.user.userId != userId)
       return true;
     try {
-      var oAccountId = this.#storage.account.accountId;
+      var oAccountId = this.storage.account.accountId;
       var nAccountId = this.fibotalkSettings.account.accountId;
-      if (Fibotalk.#isObjOrFunc(nAccountId))
+      if (Fibotalk.isObjOrFunc(nAccountId))
         return false;
       if (oAccountId && nAccountId && oAccountId != nAccountId)
         return true;
@@ -131,7 +132,7 @@ export default class Fibotalk {
    * return true only if its a JSON object
    * @param {*} obj 
    -------------------------------------*/
-  static #isObject(obj) {
+  static isObject(obj) {
     if (Array.isArray(obj))
       return false;
     if (typeof obj === "object")
@@ -143,27 +144,27 @@ export default class Fibotalk {
    * Check whether the given item is an object of a function
    * @param {*} item 
    -------------------------------------*/
-  static #isObjOrFunc(item) {
+  static isObjOrFunc(item) {
     var validObj = ['function', 'object'];
     return validObj.includes(typeof item);
   }//isObjOrFunc()
 
   /**
    * Gen new sess (id, start time).
-   * Store in #storage and in AsyncStorage.
+   * Store in storage and in AsyncStorage.
    */
-  async #genSession() {
-    this.#storage.session = {
-      sess: Fibotalk.#genId(conf.sidLen),
+  async genSession() {
+    this.storage.session = {
+      sess: Fibotalk.genId(conf.sidLen),
       ts: new Date(),
     };
-    return await this.#store("set");
+    return await this.store("set");
   }
 
   /**
    * Delete the current data and exit
    */
-  #exit() {
+  exit() {
     AsyncStorage.removeItem(conf.settings(this.appid)).then(resp => {
       console.log("Fibotalk: cleared and exiting....", resp);
     }).catch(err => {
@@ -176,10 +177,10 @@ export default class Fibotalk {
    * @param {*} action : (set/get)
    * @returns 
    */
-  async #store(action) {
+  async store(action) {
     switch (action) {
       case "set":
-        return await AsyncStorage.setItem(conf.settings(this.appid), JSON.stringify(this.#storage));
+        return await AsyncStorage.setItem(conf.settings(this.appid), JSON.stringify(this.storage));
       case "get":
       default:
         return await JSON.parse(AsyncStorage.getItem(conf.settings(this.appid)));
@@ -190,7 +191,7 @@ export default class Fibotalk {
    * generate random ID of length $len
    * @param {*} len 
   -------------------------------------*/
-  static #genId(len) {
+  static genId(len) {
     var result = '';
     var chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
     for (var i = len; i > 0; --i)
@@ -198,8 +199,8 @@ export default class Fibotalk {
     return result;
   }//genId()
 
-  async static #request(apiObj) {
-    let xhr = Fibotalk.#xhr;
+  static request(apiObj) {
+    let xhr = Fibotalk.xhr;
     return new Promise(function (resolve, reject) {
       if (!(apiObj.url && apiObj.method))
         return reject("URL not provided");
@@ -250,46 +251,46 @@ export default class Fibotalk {
    */
   setEvent(name, dimensions) {
     try {
-      if (!Fibotalk.#isObject(this.fibotalkSettings))
+      if (!Fibotalk.isObject(this.fibotalkSettings))
         delete this.fibotalkSettings;
       if (this.fibotalkSettings.user) {
-        this.#storage.user = this.#storage.user || {};
-        Object.assign(this.#storage.user, this.fibotalkSettings.user);
+        this.storage.user = this.storage.user || {};
+        Object.assign(this.storage.user, this.fibotalkSettings.user);
         delete this.fibotalkSettings.user;
       }
       if (this.fibotalkSettings.account) {
-        this.#storage.account = this.#storage.account || {};
-        Object.assign(this.#storage.account, this.fibotalkSettings.account);
+        this.storage.account = this.storage.account || {};
+        Object.assign(this.storage.account, this.fibotalkSettings.account);
         delete this.fibotalkSettings.account;
       }
-      Object.assign(this.#storage.user, this.fibotalkSettings);
+      Object.assign(this.storage.user, this.fibotalkSettings);
     } catch (error) { }
     let event = {
       event: name,
       gid: this.appid,
-      uid: this.#storage.uid,
-      sess: this.#storage.session.sess,
+      uid: this.storage.uid,
+      sess: this.storage.session.sess,
       ts: new Date(),
-      ui: this.#storage.user,
-      account: this.#storage.account,
-      ...Fibotalk.#device,
+      ui: this.storage.user,
+      account: this.storage.account,
+      ...Fibotalk.device,
     };
-    if (dimensions && Fibotalk.#isObject(dimensions)) {
+    if (dimensions && Fibotalk.isObject(dimensions)) {
       for (let i in dimensions) {
         event[`dimensions#${i}`] = dimensions[i];
       }
     }
-    event["sessDur"] = new Date(event.ts).getTime() - new Date(this.#storage.session.ts).getTime();
-    if (this.#storage.lastEventTs)
-      event["durDiff"] = new Date(event.ts).getTime() - new Date(this.#storage.lastEventTs).getTime();
+    event["sessDur"] = new Date(event.ts).getTime() - new Date(this.storage.session.ts).getTime();
+    if (this.storage.lastEventTs)
+      event["durDiff"] = new Date(event.ts).getTime() - new Date(this.storage.lastEventTs).getTime();
     else
       event["durDiff"] = 0;
-    this.#storage.lastEventTs = event.ts;
+    this.storage.lastEventTs = event.ts;
 
     console.log("Fibotalk: current_event", JSON.stringify(event));
-    this.#store("set").then(resp => {});
+    this.store("set").then(resp => { });
 
-    Fibotalk.#request({
+    Fibotalk.request({
       url: conf.apiServer + conf.eventsSync,// events sync API
       headers: {
         auth: this.appid
